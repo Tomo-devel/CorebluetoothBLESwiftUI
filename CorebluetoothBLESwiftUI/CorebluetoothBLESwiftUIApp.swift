f9ac03cdc46e5d36c0858b854df350744acdb3aa
Inoue <21cm0106@jec.ac.jp>
2023-01-08 15:18:48 +0900

    Initial Commit

---
+++ CorebluetoothBLESwiftUI/CorebluetoothBLESwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CorebluetoothBLESwiftUIApp.swift
+//  CorebluetoothBLESwiftUI
+//
+//  Created by cmStudent on 2023/01/08.
+//
+
+import SwiftUI
+
+@main
+struct CorebluetoothBLESwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
